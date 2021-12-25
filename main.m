126e44274cbef486df180a895d2d57ef77276a1d
Daniel-home-pc <daniel0dillu@gmail.com>
2021-12-25 18:51:06 +0530

    Closed file name main.m

--- main.m
+++ main.m
@@ -1,1 +1,10 @@
-img = imread('
+clc;
+tic
+img = imread('C:\Users\danie\Downloads\Image Set\DIP3E_Original_Images_CH09\Fig0944(a)(calculator).tif');
+sizeval = [567, 680];
+stegoText = 'This is a test image';
+text2char = char(stegoText);
+
+img2reduced = imresize(img, sizeval);
+imshow(img2reduced);
+toc
