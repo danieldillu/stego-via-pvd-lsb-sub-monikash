8a0f84b1bb960036cd61ca530ab502d505faa673
Daniel <daniel0dillu@gmail.com>
2021-12-26 01:07:53 +0530

    Closing the day, added some of the code for string to char change

--- main.m
+++ main.m
@@ -3,8 +3,11 @@ tic
 img = imread('C:\Users\danie\Downloads\Image Set\DIP3E_Original_Images_CH09\Fig0944(a)(calculator).tif');
 sizeval = [567, 680];
 stegoText = 'This is a test image';
-text2char = char(stegoText);
-
+sz = length(text2char);
+arrayChar = zeros(sz);
+for i=1:sz;
+    arrayChar{i} = asciidecode(text2char{i});
+end
 img2reduced = imresize(img, sizeval);
 imshow(img2reduced);
 toc
