f7f4f647cd4983ee28015c7325f85e9d164fe0aa
Daniel <daniel0dillu@gmail.com>
2021-12-26 23:57:04 +0530

    changed the characters conversion to ASCII

--- main.m
+++ main.m
@@ -3,10 +3,11 @@ tic
 img = imread('C:\Users\danie\Downloads\Image Set\DIP3E_Original_Images_CH09\Fig0944(a)(calculator).tif');
 sizeval = [567, 680];
 stegoText = 'This is a test image';
-sz = length(text2char);
+sz = length(stegoText);
 arrayChar = zeros(sz);
+% print(stegoText(1));
 for i=1:sz;
-    arrayChar{i} = asciidecode(text2char{i});
+    arrayChar(i) = int16(stegoText(i));
 end
 img2reduced = imresize(img, sizeval);
 imshow(img2reduced);
