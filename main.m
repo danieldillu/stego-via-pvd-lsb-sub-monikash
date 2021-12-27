f48eebe7ed42f68f056069bce810504fc8f5c9e7
Daniel <daniel0dillu@gmail.com>
2021-12-27 23:44:49 +0530

    String character conversion to binary successful.

--- main.m
+++ main.m
@@ -11,6 +11,9 @@ sz = length(stegoText);
 
 maxbit = 0; minbit = 256;
 for j=1:sz
+    if (int16(stegoText(j))==32)
+        int16(stegoText(j))
+    end
     if length(de2bi(int16(stegoText(j))))>maxbit
         maxbit = length(de2bi(int16(stegoText(j))));
     end
@@ -18,34 +21,28 @@ for j=1:sz
         minbit = length(de2bi(int16(stegoText(j))));
     end
 end
+% To find the maximum and minimum bits of the characters being entered as
+% Stego Text
 binarray = zeros(sz,maxbit);
 for i=1:maxbit;
     for j=1:sz;
         binarray(j,i) = 4;
     end
 end
+% Initialise a number other than 0 and 1 in array
 for i=1:sz;
     bintemp = de2bi(int16(stegoText(i)));
-    j=1; 
+    j=1; count = 0;
     sizeofbinaryarray = length(bintemp);
-    if sizeofbinaryarray < maxbit
-        differencebit = maxbit - sizeofbinaryarray;
-        j = j + differencebit;
-    end
     k = 1;
     while j<=length(bintemp)
-        
-%         binarray(i,j) = bintemp(:,k)
-        i
-        j
-        k
+        binarray(i,j) = bintemp(k);
         j=j+1;
         k=k+1;
-        
-        
-    end
-    
+    end 
 end
+% Actual conversion from decimal ASCII values to binary 
 img2reduced = imresize(img, sizeval);
 % imshow(img2reduced);
+imshow(binarray);
 toc
