81c6943dfd1b1fe14c59f96964adb061e31ab723
Daniel <daniel0dillu@gmail.com>
2021-12-27 19:02:32 +0530

    Conversion of ASCII Value to binary is been performed so far yt some of the issue are not yet resolved. Working on it. Major problem of the how loops will work is performing well.

--- main.m
+++ main.m
@@ -1,14 +1,51 @@
 clc;
+clear all;
 tic
 img = imread('C:\Users\danie\Downloads\Image Set\DIP3E_Original_Images_CH09\Fig0944(a)(calculator).tif');
 sizeval = [567, 680];
 stegoText = 'This is a test image';
 sz = length(stegoText);
-arrayChar = zeros(sz);
+
+% arrayChar = zeros(sz);
 % print(stegoText(1));
+
+maxbit = 0; minbit = 256;
+for j=1:sz
+    if length(de2bi(int16(stegoText(j))))>maxbit
+        maxbit = length(de2bi(int16(stegoText(j))));
+    end
+    if length(de2bi(int16(stegoText(j))))<maxbit
+        minbit = length(de2bi(int16(stegoText(j))));
+    end
+end
+binarray = zeros(sz,maxbit);
+for i=1:maxbit;
+    for j=1:sz;
+        binarray(j,i) = 4;
+    end
+end
 for i=1:sz;
-    arrayChar(i) = int16(stegoText(i));
+    bintemp = de2bi(int16(stegoText(i)));
+    j=1; 
+    sizeofbinaryarray = length(bintemp);
+    if sizeofbinaryarray < maxbit
+        differencebit = maxbit - sizeofbinaryarray;
+        j = j + differencebit;
+    end
+    k = 1;
+    while j<=length(bintemp)
+        
+%         binarray(i,j) = bintemp(:,k)
+        i
+        j
+        k
+        j=j+1;
+        k=k+1;
+        
+        
+    end
+    
 end
 img2reduced = imresize(img, sizeval);
-imshow(img2reduced);
+% imshow(img2reduced);
 toc
