eebf0b8cc62a2df5557e77893eeea57a73cb62c8
Daniel <daniel0dillu@gmail.com>
2022-01-03 16:29:05 +0530

    STR2BIN fine, Pixel Quantize is working charstr2bin is added, image pixel traversal is okay. Difference is not yet handled, need to fix.

---
+++ charStringtoBin.m
@@ -0,0 +1,34 @@
+function [ binarray ] = charStringtoBin( stegoText )
+%charStringtoBin convert string of characters to binary returns a 2D array
+%   It takes a input of String of charcacters, traverses the characters and
+%   converts to binary in row-wise. It should be noted that the bit length
+%   of the ASCII value is not always equals, it may be 6 bit or more. This
+%   is returned end of the function.
+sz = length(stegoText);
+maxbit = 0;
+for j=1:sz
+    if length(de2bi(int16(stegoText(j))))>maxbit
+        maxbit = length(de2bi(int16(stegoText(j))));
+    end
+end
+% To find the maximum and minimum bits of the characters being entered as
+% Stego Text
+binarray = zeros(sz,maxbit);
+for i=1:maxbit;
+    for j=1:sz;
+        binarray(j,i) = 4;
+    end
+end
+% Initialise a number other than 0 and 1 in array, to identify whether it
+% is a bit. Here we took it as 4.
+for i=1:sz;
+    bintemp = de2bi(int16(stegoText(i)));
+    j=1;
+    k = 1;
+    while j<=length(bintemp)
+        binarray(i,j) = bintemp(k);
+        j=j+1;
+        k=k+1;
+    end
+end
+end
--- main.m
+++ main.m
@@ -2,46 +2,65 @@ clc;
 clear all;
 tic
 PRootURL = 'C:\Users\danie\OneDrive\Documents\DIP Projects Others\Monika Sharma DIP Project\algo';
-ImageURL = strcat(PRootURL,'\images\','DIP3E_Original_Images_CH06\Fig0631(a)(strawberries_coffee_full_color).tif');
+ImageDirURL = 'C:\Users\danie\OneDrive\Documents\DIP Projects Others\Monika Sharma DIP Project\algo\test image set\DIP3E_Original_Images_CH06\';
+ImageURL = strcat(ImageDirURL,'Fig0631(a)(strawberries_coffee_full_color).tif');
 img = imread(ImageURL);
-sizeval = [567, 680];
 stegoText = 'This is a test image';
-sz = length(stegoText);
+str2binary = charStringtoBin (stegoText);
+% Actual conversion from decimal ASCII values of chars of string to binary
+img2reduced = imresize(img, [567, 680]);
+% Larger image is resized to a 567 x 680, why? No reasons.
+[rows, columns, NoOfchannels] = size(img2reduced);
+% To get the dimensions of the image, NoOfchannels is used to fined if is a
+% grayscale.
+if(NoOfchannels == 3)
+    graysImg = rgb2gray(img2reduced);
+else
+    graysImg = img2reduced;
+end
+dimenGrayImg = size(graysImg);
+% If it a RGB image it is converted to Grayscale image, else it is assigned
+% as Grayscale.
+% ------------------------------Quantization----
+% nonOverlappingBlockWithDiff
+blocksize = dimenGrayImg(1)*(dimenGrayImg(2)/2);
+k=1; nonOverlappingBlockWithDiff = zeros(blocksize ,5);
+for i=1:dimenGrayImg(1)
+    if mod(i,2)~=0
+        for j=1:2:dimenGrayImg(2)
+            tempi = graysImg(i,j);
+            tempip1 = graysImg(i,j+1);
+            nonOverlappingBlockWithDiff(k,1) = i;
+            nonOverlappingBlockWithDiff(k,2) = j;
+            nonOverlappingBlockWithDiff(k,3) = graysImg(i,j);
+            nonOverlappingBlockWithDiff(k,4) =  graysImg(i,j+1);            
+            if tempi > tempip1
+                nonOverlappingBlockWithDiff(k,5) = (tempi - tempip1);
+            else
+                nonOverlappingBlockWithDiff(k,5) = tempip1- tempi;
+            end
+        %       To get a absolute of the difference becoz for negative value it is
+        %       taking zero instead of any number.
+            k=k+1;
+        end
+    else
+        for j=dimenGrayImg(2):-2:1
+            tempi = graysImg(i,j);
+            tempip1 = graysImg(i,j-1);
+            nonOverlappingBlockWithDiff(k,1) = i;
+            nonOverlappingBlockWithDiff(k,2) = j;
+            nonOverlappingBlockWithDiff(k,3) = graysImg(i,j);
+            nonOverlappingBlockWithDiff(k,4) =  graysImg(i,j-1);
+            if tempi > tempip1
+                nonOverlappingBlockWithDiff(k,5) = (tempi - tempip1);
+            else
+                nonOverlappingBlockWithDiff(k,5) = tempip1- tempi;
+            end
+        %       To get a absolute of the difference becoz for negative value it is
+        %       taking zero instead of any number.
+            k=k+1;
+        end        
+    end
+end
 
-% arrayChar = zeros(sz);
-% print(stegoText(1));
-
-maxbit = 0; minbit = 256;
-for j=1:sz
-    if length(de2bi(int16(stegoText(j))))>maxbit
-        maxbit = length(de2bi(int16(stegoText(j))));
-    end
-    if length(de2bi(int16(stegoText(j))))<maxbit
-        minbit = length(de2bi(int16(stegoText(j))));
-    end
-end
-% To find the maximum and minimum bits of the characters being entered as
-% Stego Text
-binarray = zeros(sz,maxbit);
-for i=1:maxbit;
-    for j=1:sz;
-        binarray(j,i) = 4;
-    end
-end
-% Initialise a number other than 0 and 1 in array
-for i=1:sz;
-    bintemp = de2bi(int16(stegoText(i)));
-    j=1; count = 0;
-    sizeofbinaryarray = length(bintemp);
-    k = 1;
-    while j<=length(bintemp)
-        binarray(i,j) = bintemp(k);
-        j=j+1;
-        k=k+1;
-    end
-end
-% Actual conversion from decimal ASCII values to binary
-img2reduced = imresize(img, sizeval);
-% imshow(img2reduced);
-% imshow(binarray);
 toc
