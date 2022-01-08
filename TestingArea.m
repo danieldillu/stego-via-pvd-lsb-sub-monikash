1c6c7426e2b6d24cf1dcfbd9de9e55b6dfeb9e2e
Daniel <daniel0dillu@gmail.com>
2022-01-08 18:12:30 +0530

    PDMO is done, APVD working

--- TestingArea.m
+++ TestingArea.m
@@ -19,25 +19,26 @@ gR = img2reduced (1,3);
 rangeMat = [0 31 round(log2(31-0)-2);
     32 255 round(log2(255-32)-3)];
 % ---------------------------------------------------
+% PDMO starts
 dLC = abs(gL - gC);
 t = rangeMat(1,3);
 t1 = t;
 t2 = t;
-if dLC > rangeMat(1,1) && dLC > rangeMat(1,2)
+if belongsin(dLC,rangeMat(1,1),rangeMat(1,2))
     rmd1 = mod(gL,8);
-elseif dLC > rangeMat(2,1) && dLC > rangeMat(2,2)
+elseif belongsin(dLC,rangeMat(2,1),rangeMat(2,2))
     rmd1 = mod(gL,16);
 end
 
-if dLC > rangeMat(1,1) && dLC > rangeMat(1,2)
+if belongsin(dLC,rangeMat(1,1),rangeMat(1,2))
     rmd2 = mod(gC,8);
-elseif dLC > rangeMat(2,1) && dLC > rangeMat(2,2)
+elseif belongsin(dLC,rangeMat(2,1),rangeMat(2,2))
     rmd2 = mod(gC,16);
 end
 
 d1 = dec( t1);
 d2 = dec( t2);
-% %  Above need corrections 
+% %  Above need corrections
 dv1 = rmd1 - d1;
 dv2 = d1 - rmd1;
 dv3 = rmd2 - d2;
@@ -46,9 +47,9 @@ dv4 = d2 - rmd2;
 % gLdash
 
 if rmd1 == d1
-   gLdash = gL; 
+    gLdash = gL;
 elseif rmd1 < d1 && abs(dv1) < power(2,t-1)
-    gLdash = gL - dv1;    
+    gLdash = gL - dv1;
 elseif rmd1 > d1 && abs(dv2) < power(2,t-1)
     gLdash = gL + dv2;
 elseif rmd1 < d1 && abs(dv1) >= power(2,t-1)
@@ -76,8 +77,57 @@ elseif rmd2 > d2 && abs(dv4) >= power(2,t-1)
     ee = power(2,t) + dv4;
     gCdash = gC + ee;
 else
-    disp('Something wrong on line 62 of function');
+    disp('Something wrong on line in gCdash block of function');
 end
+
+% for gCstr , gLstr
+dLCdash = abs(gLdash - gCdash);
+
+if belongsin(dLC,rangeMat(1,1),rangeMat(1,2))
+    %     Take eqn 26
+    if belongsin(dLCdash,rangeMat(1,1),rangeMat(1,2))
+        gLstr = gLdash;
+        gCstr = gCdash;
+    elseif belongsin(dLCdash,rangeMat(2,1),rangeMat(2,2)) && gLdash >= gCdash
+        gLstr = gLdash - power(2,t);
+        gCstr = gCdash + power(2,t);
+    elseif belongsin(dLCdash,rangeMat(2,1),rangeMat(2,2)) && gLdash < gCdash
+        gLstr = gLdash + power(2,t);
+        gCstr = gCdash - power(2,t);
+    else
+        disp('Something wrong on line in gCstr , gLstr block of function');
+    end
+elseif belongsin(dLC,rangeMat(2,1),rangeMat(2,2))
+    if belongsin(dLCdash,rangeMat(2,1),rangeMat(2,2))
+        gLstr = gLdash;
+        gCstr = gCdash;
+    elseif belongsin(dLCdash,rangeMat(1,1),rangeMat(1,2)) && gLdash >= gCdash
+        gLstr = gLdash + power(2,t);
+        gCstr = gCdash - power(2,t);
+    elseif belongsin(dLCdash,rangeMat(1,1),rangeMat(1,2)) && gLdash < gCdash
+        gLstr = gLdash - power(2,t);
+        gCstr = gCdash + power(2,t);
+    else
+        disp('Something wrong on line in gCstr , gLstr block of function');
+    end
+else
+    disp('Something wrong on line in gCdash block of function');
+end
+
+if gLstr < 0 || gCstr < 0
+    gLstr = gLstr + power(2,t);
+    gCstr = gCstr + power(2,t);
+elseif gLstr > 255 || gCstr > 255
+    gLstr = gLstr - power(2,t);
+    gCstr = gCstr - power(2,t);
+else
+%     no need this;    
+end
+
+% PDMO ends
+
+% APVD starts.
+gAVG = (gLstr + gCstr)/2;
 
 % ---------------------------------------------------
 
