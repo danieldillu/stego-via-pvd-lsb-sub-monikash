bb64706ffd4ecb3d5661a3beeae9170bb738135a
Daniel <daniel0dillu@gmail.com>
2022-01-11 00:45:38 +0530

    Doc file created.

--- xtraction.m
+++ xtraction.m
@@ -1,6 +1,8 @@
 function [ finalArray ] = xtraction( gLstr,gCstr,gRstr)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
+%xtraction() is for executing the extraction binary message bits.
+%   It is used to extraction the bits which was initially embedding in the image.
+%   It takes an arguments list 3 Image Pixels in grayscale or single component of RGB image.
+
 rangeMat = [0 31 round(log2(31-0)-2);
     32 255 round(log2(255-32)-3)];
 
