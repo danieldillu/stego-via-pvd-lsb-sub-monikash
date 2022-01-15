f36e861e6cb2df296e99d4945bad3ed5941f0f8f
Daniel <daniel0dillu@gmail.com>
2022-01-15 19:34:21 +0530

    Full Image work continued

--- executionSec.m
+++ executionSec.m
@@ -1,10 +1,68 @@
-% gL = 127;
-% gC = 125;
-% gR = 123;
-% rangeMat = [0 31 round(log2(31-0)-2);
-%     32 255 round(log2(255-32)-3)];
-% str2binary = [1 1 0 0 1 0 0 1 0 0 1 1];
-% startptofstr = 1;
-% [ gLstr,gCstr,gR,endpt ] = pdmo( gL,gC,gR,rangeMat,str2binary,startptofstr );
-% [ gLstr,gCstr,gRstr ] = avgpvd( gLstr,gCstr,gR,str2binary,endpt+1 )
+clc;
+clear all;
+tic
+PRootURL = 'C:\Users\danie\OneDrive\Documents\DIP Projects Others\Monika Sharma DIP Project\algo';
+ImageDirURL = 'C:\Users\danie\OneDrive\Documents\DIP Projects Others\Monika Sharma DIP Project\algo\test image set\DIP3E_Original_Images_CH06\';
+ImageURL = strcat(ImageDirURL,'Fig0631(a)(strawberries_coffee_full_color).tif');
+img = imread(ImageURL);
+stegoText = 'This is a test image';
+str2binary = charStringtoBin (stegoText);
+% Actual conversion from decimal ASCII values of chars of string to binary
+img2reduced = imresize(img, [567, 680]);
+% Larger image is resized to a 567 x 680, why? No reasons.
+[rows, columns, NoOfchannels] = size(img2reduced);
+% To get the dimensions of the image, NoOfchannels is used to fined if is a
+% grayscale.
+if(NoOfchannels == 3)
+    graysImg = rgb2gray(img2reduced);
+else
+    graysImg = img2reduced;
+end
+dimenGrayImg = size(graysImg);
+% If it a RGB image it is converted to Grayscale image, else it is assigned
+% as Grayscale.
+% ------------------------------Quantization----
+% nonOverlappingBlockWithDiff
+blocksize = dimenGrayImg(1)*(dimenGrayImg(2)/2);
+k=1; nonOverlappingBlockWithDiff = zeros(blocksize ,5);
+for i=1:dimenGrayImg(1)
+    if mod(i,2)~=0
+        for j=1:2:dimenGrayImg(2)
+            disp([i,j]);
+%             tempi = graysImg(i,j);
+%             tempip1 = graysImg(i,j+1);
+%             nonOverlappingBlockWithDiff(k,1) = i;
+%             nonOverlappingBlockWithDiff(k,2) = j;
+%             nonOverlappingBlockWithDiff(k,3) = graysImg(i,j);
+%             nonOverlappingBlockWithDiff(k,4) =  graysImg(i,j+1);            
+%             if tempi > tempip1
+%                 nonOverlappingBlockWithDiff(k,5) = (tempi - tempip1);
+%             else
+%                 nonOverlappingBlockWithDiff(k,5) = tempip1- tempi;
+%             end
+%         %       To get a absolute of the difference becoz for negative value it is
+%         %       taking zero instead of any number.
+%             k=k+1;
+        end
+    else
+        for j=dimenGrayImg(2):-2:1
+            disp([i,j]);
+%             tempi = graysImg(i,j);
+%             tempip1 = graysImg(i,j-1);
+%             nonOverlappingBlockWithDiff(k,1) = i;
+%             nonOverlappingBlockWithDiff(k,2) = j;
+%             nonOverlappingBlockWithDiff(k,3) = graysImg(i,j);
+%             nonOverlappingBlockWithDiff(k,4) =  graysImg(i,j-1);
+%             if tempi > tempip1
+%                 nonOverlappingBlockWithDiff(k,5) = (tempi - tempip1);
+%             else
+%                 nonOverlappingBlockWithDiff(k,5) = tempip1- tempi;
+%             end
+%         %       To get a absolute of the difference becoz for negative value it is
+%         %       taking zero instead of any number.
+%             k=k+1;
+        end        
+    end
+end
 
+toc
--- reverspdmo.m
+++ reverspdmo.m
@@ -1,16 +1,16 @@
 function [ finalbinarray ] = reverspdmo( gLstr,gCstr,rangeMat )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-ds = gLstr - gCstr ;
+ds = abs(gLstr - gCstr );
 numOfBitsx2 = -1; numOfBitsx1 = -1;
 if belongsin(ds,rangeMat(1,1),rangeMat(1,2))
     rMd1str = mod(gLstr,8);
-    numOfBitsx1 = 3;
+    numOfBitsx1 = rangeMat(1,3);
 elseif belongsin(ds,rangeMat(2,1),rangeMat(2,2))
     rMd1str = mod(gLstr,16);
-    numOfBitsx1 = 4;
+    numOfBitsx1 = rangeMat(2,3);
 else
-    disp('Somthing wrong');
+    disp('reverspdmo:13 Somthing wrong');
 end
 
 if belongsin(ds,rangeMat(1,1),rangeMat(1,2))
@@ -20,7 +20,7 @@ elseif belongsin(ds,rangeMat(2,1),rangeMat(2,2))
     xrMd2str = mod(gCstr,16);
     numOfBitsx2 = 4;
 else
-    disp('Somthing wrong');
+    disp('reverspdmo:23 Somthing wrong');
 end
 [numOfBitsx1, rMd1str,numOfBitsx2,xrMd2str];
 
